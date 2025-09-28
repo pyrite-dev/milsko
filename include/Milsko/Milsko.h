85d6807592a2b5f63667956ab9ccdde0c42cf96f
nishi <nishi@b9cfdab3-6d41-4d17-bbe4-086880011989>
2025-09-28 02:51:15 +0000

    stuff
    git-svn-id: http://svn2.nishi.boats/svn/milsko/trunk@3 b9cfdab3-6d41-4d17-bbe4-086880011989

--- include/Milsko/Milsko.h
+++ include/Milsko/Milsko.h
@@ -2,5 +2,6 @@
 #ifndef __MILSKO_MILSKO_H__
 #define __MILSKO_MILSKO_H__
 
+#include <Milsko/LowLevel.h>
 
 #endif
---
+++ include/Milsko/TypeDef.h
@@ -0,0 +1,10 @@
+/* $Id$ */
+#ifndef __MILSKO_TYPEDEF_H__
+#define __MILSKO_TYPEDEF_H__
+
+typedef struct _MilskoPoint {
+	int x;
+	int y;
+} MilskoPoint;
+
+#endif
