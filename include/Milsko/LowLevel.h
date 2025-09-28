1beb177f328e85e1c07dcad435fbda2a43b5c576
nishi <nishi@b9cfdab3-6d41-4d17-bbe4-086880011989>
2025-09-28 09:55:42 +0000

    aa
    git-svn-id: http://svn2.nishi.boats/svn/milsko/trunk@20 b9cfdab3-6d41-4d17-bbe4-086880011989

--- include/Milsko/LowLevel.h
+++ include/Milsko/LowLevel.h
@@ -17,16 +17,22 @@ typedef void* MilskoLLColor;
 #include <Milsko/MachDep.h>
 #include <Milsko/TypeDefs.h>
 
-MILSKODECL MilskoLL	 MilskoLLCreate(MilskoLL parent, int x, int y, int width, int height);
-MILSKODECL void		 MilskoLLDestroy(MilskoLL handle);
-MILSKODECL void		 MilskoLLPolygon(MilskoLL handle, MilskoPoint* points, int points_count, MilskoLLColor color);
+MILSKODECL MilskoLL MilskoLLCreate(MilskoLL parent, int x, int y, int width, int height);
+MILSKODECL void	    MilskoLLDestroy(MilskoLL handle);
+
+MILSKODECL void MilskoLLPolygon(MilskoLL handle, MilskoPoint* points, int points_count, MilskoLLColor color);
+
 MILSKODECL MilskoLLColor MilskoLLAllocColor(MilskoLL handle, int r, int g, int b);
-MILSKODECL void		 MilskoLLGetXYWH(MilskoLL handle, int* x, int* y, unsigned int* w, unsigned int* h);
-MILSKODECL void		 MilskoLLSetXY(MilskoLL handle, int x, int y);
-MILSKODECL void		 MilskoLLSetWH(MilskoLL handle, int w, int h);
-MILSKODECL void		 MilskoLLSetTitle(MilskoLL handle, const char* title);
-MILSKODECL int		 MilskoLLPending(MilskoLL handle);
-MILSKODECL void		 MilskoLLNextEvent(MilskoLL handle);
-MILSKODECL void		 MilskoLLSleep(int ms);
+MILSKODECL void		 MilskoLLFreeColor(MilskoLLColor color);
+
+MILSKODECL void MilskoLLGetXYWH(MilskoLL handle, int* x, int* y, unsigned int* w, unsigned int* h);
+MILSKODECL void MilskoLLSetXY(MilskoLL handle, int x, int y);
+MILSKODECL void MilskoLLSetWH(MilskoLL handle, int w, int h);
+
+MILSKODECL void MilskoLLSetTitle(MilskoLL handle, const char* title);
+
+MILSKODECL int	MilskoLLPending(MilskoLL handle);
+MILSKODECL void MilskoLLNextEvent(MilskoLL handle);
+MILSKODECL void MilskoLLSleep(int ms);
 
 #endif
