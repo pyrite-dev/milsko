64534fe71225af954bb8418b13c6a227f74c2bea
NishiOwO <nishi@nishi.boats>
2025-09-30 11:43:15 +0000

    document
    git-svn-id: http://svn2.nishi.boats/svn/milsko/trunk@76 b9cfdab3-6d41-4d17-bbe4-086880011989

--- tools/doc.pl
+++ tools/doc.pl
@@ -6,8 +6,34 @@ our $title = "Milsko GUI Toolkit Documentation";
 our @pathlist = ("include");
 our @notes = ("warning", "unsure", "note");
 
+sub sentence {
+	my $str = $_[0];
+	$str =~ s/([^\.])$/\1./g;
+	$str =~ s/`([^`]+)`/<code>\1<\/code>/g;
+	return $str;
+}
+
 sub out {
 	$html = $html . "$_[0]\n";
+}
+
+sub arguments {
+	my ($args) = @_;
+	my $out = "";
+	my @arglist = split(/[ \t]*,[ \t]*/, $args);
+
+	$out = $out . " (\n";
+	foreach my $str (@arglist){
+		$out = $out . "\t$str";
+		if($str eq $arglist[0+@arglist - 1]){
+			$out = $out . "\n";
+		}else{
+			$out = $out . ",\n";
+		}
+	}
+	$out = $out . ")";
+
+	return $out;
 }
 
 sub scan_dir {
@@ -31,30 +57,45 @@ sub scan_dir {
 		my $file = 0;
 		my $has_file = 0;
 		my %kv = ();
+		my %param = ();
+		my @paramlist = ();
 
 		print(STDERR "$path is a file\n");
 		open(IN, "<", "$path");
 		while(my $l = <IN>){
+			my $l2 = $l;
 			$l =~ s/[\r\n]+$//g;
+
+			$l2 =~ s/[\r\n \t]+//g;
+			if(length($l2) == 0){
+				next;
+			}
+
 			if($out){
 				my $para = "p";
 				my $brief = $kv{brief} or "";
 
-				$brief =~ s/([^\.])$/\1./g;
+				$brief = sentence($brief);
 
 				$out = 0;
 				if(!$has_file){
 					print(STDERR "Warning: missing comment with %file, ignoring\n");
 					next;
 				}
+				if(!defined($kv{brief})){
+					print(STDERR "Warning: comment missing %brief, ignoring\n");
+					next;
+				}
 				if(!$file && defined($kv{brief})){
 					$para = "dd";
 
-					$l =~ s/[ \t]*(?:;|\{.+)[ \t]*$//g;
+					$l =~ s/[ \t]*\\$//g;
+					$l =~ s/[ \t]*(?:;|\{.+)[ \t]*$/;/g;
+					if(!($l =~ /^#/)){
+						$l =~ s/\(([^\)]+)\)/arguments($1)/ge;
+					}
 
-					out("<dt>");
-					out("	<code>$l;</code>");
-					out("</dt>");
+					out("<pre><code>$l</code></pre>");
 				}
 				out("<$para>");
 				out("	$brief");
@@ -66,18 +107,36 @@ sub scan_dir {
 						out("	<img src=\"$note.gif\" alt=\"$note\">");
 						out("</dt>");
 						out("<dd>");
-						out("	$kv{$note}");
+						out("	" . sentence($kv{$note}));
 						out("</dd>");
 					}
 				}
-				if(!$file){
-					out("<br>");
+
+				foreach my $p (@paramlist){
+					out("<dt>");
+					out("	Parameter <code>$p</code>");
+					out("</dt>");
+					out("<dd>");
+					out("	" . sentence($param{$p}));
+					out("</dd>");
 				}
+				
+				if(defined($kv{return})){
+					out("<dt>");
+					out("	Returns");
+					out("</dt>");
+					out("<dd>");
+					out("	" . sentence($kv{return}));
+					out("</dd>");
+				}
+				out("<hr>");
 			}elsif($l =~ /^([ \t]*)\/\*\!/){
 				$spaces = $1;
 				$in = 1;
 				$file = 0;
 				%kv = ();
+				%param = ();
+				@paramlist = ();
 			}elsif($in && ($l =~ /^$spaces \*\//)){
 				$in = 0;
 				$out = 1;
@@ -85,12 +144,17 @@ sub scan_dir {
 				$kv{$1} = $2;
 				if($1 eq "file"){
 					if(!$has_file){
-						out("<hr>");
-						out("<h2>$2</h2>");
+						out("<h2 align=\"center\">$2</h2>");
 					}
 
 					$file = 1;
 					$has_file = 1;
+				}elsif($1 eq "param"){
+					my $p = $2;
+					$p =~ /^([^ ]+)(?:[ \t]+(.+))?$/;
+
+					$param{$1} = $2;
+					push(@paramlist, $1);
 				}
 			}
 		}
@@ -105,6 +169,7 @@ out("		<title>$title</title>");
 out("	</head>");
 out("	<body>");
 out("		<h1 align=\"center\">$title</h1>");
+out("		<hr>");
 
 foreach my $f (@pathlist){
 	scan_dir($f, $f);
