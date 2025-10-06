5fe14bb6ad7c05ee964508754ed1702f5ff8a506
nishi <nishi@b9cfdab3-6d41-4d17-bbe4-086880011989>
2025-10-06 12:22:30 +0000

    cursor
    git-svn-id: http://svn2.nishi.boats/svn/milsko/trunk@182 b9cfdab3-6d41-4d17-bbe4-086880011989

--- src/cursor/cross.c
+++ src/cursor/cross.c
@@ -2,43 +2,55 @@
 #include <Mw/Milsko.h>
 
 /**
- * Created by bitmaptobdf
- *
  * Copyright notice:
- *   These ""glyphs"" are unencumbered
+ *   Copyright (c) Digital Equipment Corporation,1988. All Rights Reserved.
+ */
+/**
+ *  Permission to use, copy, modify, and distribute this software and
+ *  its documentation for any purpose and without fee is hereby granted,
+ *  provided that the above copyright notices appear in all copies and
+ *  that both those copyright notices and this permission notice appear
+ *  in supporting documentation, and that the name
+ *  Digital Equipment Corporation not be used in advertising or
+ *  publicity pertaining to distribution of the software without
+ *  specific, written prior permission.  Digital
+ *  Equipment Corporation makes no representations about the suitability
+ *  of this software for any purpose.  It is provided "as is" without
+ *  express or implied warranty.
+ *  DIGITAL EQUIPMENT CORPORATION DISCLAIMS ALL
+ *  WARRANTIES WITH REGARD TO THIS SOFTWARE, INCLUDING ALL IMPLIED
+ *  WARRANTIES OF MERCHANTABILITY AND FITNESS, IN NO EVENT SHALL
+ *  DIGITAL EQUIPMENT CORPORATION BE LIABLE FOR ANY SPECIAL,
+ *  INDIRECT OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER
+ *  RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF
+ *  CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN
+ *  CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 MwCursor MwCursorCross = {
-    14, 14, -6, -8, {14343, /* ###........### */
-		     15375, /* ####......#### */
-		     15903, /* #####....##### */
-		     7998,  /* .#####..#####. */
-		     4092,  /* ..##########.. */
-		     2040,  /* ...########... */
-		     1008,  /* ....######.... */
-		     1008,  /* ....######.... */
-		     2040,  /* ...########... */
-		     4092,  /* ..##########.. */
-		     7998,  /* .#####..#####. */
-		     15903, /* #####....##### */
-		     15375, /* ####......#### */
-		     14343, /* ###........### */
-		     0, 0}};
+    13, 12, -6, -6, {7175, /* ###.......### */
+		     4617, /* #..#.....#..# */
+		     2322, /* .#..#...#..#. */
+		     1188, /* ..#..#.#..#.. */
+		     584,  /* ...#..#..#... */
+		     272,  /* ....#...#.... */
+		     272,  /* ....#...#.... */
+		     584,  /* ...#..#..#... */
+		     1188, /* ..#..#.#..#.. */
+		     2322, /* .#..#...#..#. */
+		     4617, /* #..#.....#..# */
+		     7175, /* ###.......### */
+		     0, 0, 0, 0}};
 MwCursor MwCursorCrossMask = {
-    16, 16, -7, -9, {
-			61455, /* ####........#### */
-			63519, /* #####......##### */
-			64575, /* ######....###### */
-			65151, /* #######..####### */
-			32766, /* .##############. */
-			16380, /* ..############.. */
-			8184,  /* ...##########... */
-			4080,  /* ....########.... */
-			4080,  /* ....########.... */
-			8184,  /* ...##########... */
-			16380, /* ..############.. */
-			32766, /* .##############. */
-			65151, /* #######..####### */
-			64575, /* ######....###### */
-			63519, /* #####......##### */
-			61455  /* ####........#### */
-		    }};
+    13, 12, -6, -6, {7175, /* ###.......### */
+		     7695, /* ####.....#### */
+		     3870, /* .####...####. */
+		     1980, /* ..####.####.. */
+		     1016, /* ...#######... */
+		     496,  /* ....#####.... */
+		     496,  /* ....#####.... */
+		     1016, /* ...#######... */
+		     1980, /* ..####.####.. */
+		     3870, /* .####...####. */
+		     7695, /* ####.....#### */
+		     7175, /* ###.......### */
+		     0, 0, 0, 0}};
--- src/cursor/default.c
+++ src/cursor/default.c
@@ -2,43 +2,65 @@
 #include <Mw/Milsko.h>
 
 /**
- * Created by bitmaptobdf
- *
  * Copyright notice:
- *   These ""glyphs"" are unencumbered
+ *   Copyright (c) Digital Equipment Corporation,1988. All Rights Reserved.
+ */
+/**
+ *  Permission to use, copy, modify, and distribute this software and
+ *  its documentation for any purpose and without fee is hereby granted,
+ *  provided that the above copyright notices appear in all copies and
+ *  that both those copyright notices and this permission notice appear
+ *  in supporting documentation, and that the name
+ *  Digital Equipment Corporation not be used in advertising or
+ *  publicity pertaining to distribution of the software without
+ *  specific, written prior permission.  Digital
+ *  Equipment Corporation makes no representations about the suitability
+ *  of this software for any purpose.  It is provided "as is" without
+ *  express or implied warranty.
+ *  DIGITAL EQUIPMENT CORPORATION DISCLAIMS ALL
+ *  WARRANTIES WITH REGARD TO THIS SOFTWARE, INCLUDING ALL IMPLIED
+ *  WARRANTIES OF MERCHANTABILITY AND FITNESS, IN NO EVENT SHALL
+ *  DIGITAL EQUIPMENT CORPORATION BE LIABLE FOR ANY SPECIAL,
+ *  INDIRECT OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER
+ *  RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF
+ *  CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN
+ *  CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 MwCursor MwCursorDefault = {
-    8, 14, 0, -14, {128, /* #....... */
-		    192, /* ##...... */
-		    224, /* ###..... */
-		    240, /* ####.... */
-		    248, /* #####... */
-		    252, /* ######.. */
-		    254, /* #######. */
-		    255, /* ######## */
-		    248, /* #####... */
-		    216, /* ##.##... */
-		    140, /* #...##.. */
-		    12,	 /* ....##.. */
-		    6,	 /* .....##. */
-		    6,	 /* .....##. */
-		    0, 0}};
+    16, 16, 0, -15, {
+			0,     /* ................ */
+			24576, /* .##............. */
+			30720, /* .####........... */
+			15872, /* ..#####......... */
+			16256, /* ..#######....... */
+			8160,  /* ...########..... */
+			8184,  /* ...##########... */
+			3968,  /* ....#####....... */
+			4032,  /* ....######...... */
+			1760,  /* .....##.###..... */
+			1648,  /* .....##..###.... */
+			568,   /* ......#...###... */
+			540,   /* ......#....###.. */
+			14,    /* ............###. */
+			4,     /* .............#.. */
+			0      /* ................ */
+		    }};
 MwCursor MwCursorDefaultMask = {
-    10, 16, -1, -15, {
-			 768,  /* ##........ */
-			 896,  /* ###....... */
-			 960,  /* ####...... */
-			 992,  /* #####..... */
-			 1008, /* ######.... */
-			 1016, /* #######... */
-			 1020, /* ########.. */
-			 1022, /* #########. */
-			 1023, /* ########## */
-			 1023, /* ########## */
-			 1016, /* #######... */
-			 956,  /* ###.####.. */
-			 828,  /* ##..####.. */
-			 30,   /* .....####. */
-			 30,   /* .....####. */
-			 12    /* ......##.. */
-		     }};
+    16, 16, 0, -15, {
+			57344, /* ###............. */
+			63488, /* #####........... */
+			65024, /* #######......... */
+			32640, /* .########....... */
+			32736, /* .##########..... */
+			16376, /* ..###########... */
+			16380, /* ..############.. */
+			8184,  /* ...##########... */
+			8160,  /* ...########..... */
+			4080,  /* ....########.... */
+			4088,  /* ....#########... */
+			1916,  /* .....###.#####.. */
+			1854,  /* .....###..#####. */
+			543,   /* ......#....##### */
+			14,    /* ............###. */
+			4      /* .............#.. */
+		    }};
