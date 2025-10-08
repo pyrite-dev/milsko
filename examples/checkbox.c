c4dd26ecd23f6f5aff077da3ef2bf39f12447fc7
NishiOwO <nishi@nishi.boats>
2025-10-08 15:20:22 +0000

    add the example
    git-svn-id: http://svn2.nishi.boats/svn/milsko/trunk@224 b9cfdab3-6d41-4d17-bbe4-086880011989

---
+++ examples/checkbox.c
@@ -0,0 +1,25 @@
+/* $Id$ */
+#include <Mw/Milsko.h>
+
+int main() {
+	MwWidget window = MwVaCreateWidget(MwWindowClass, "test", NULL, 0, 0, 8 + 16 + 8 + 16 * 10 + 8, 8 + 16 + 8 + 16 + 8,
+					   MwNtitle, "checkbox",
+					   NULL);
+
+	MwVaCreateWidget(MwCheckBoxClass, "cb1", window, 8, 8, 16, 16,
+			 NULL);
+	MwVaCreateWidget(MwCheckBoxClass, "cb2", window, 8, 8 + 16 + 8, 16, 16,
+			 MwNchecked, 1,
+			 NULL);
+
+	MwVaCreateWidget(MwLabelClass, "lab1", window, 8 + 16 + 8, 8, 16 * 10, 16,
+			 MwNtext, "lorem ipsum 1",
+			 MwNalignment, MwALIGNMENT_BEGINNING,
+			 NULL);
+	MwVaCreateWidget(MwLabelClass, "lab2", window, 8 + 16 + 8, 8 + 16 + 8, 16 * 10, 16,
+			 MwNtext, "lorem ipsum 2",
+			 MwNalignment, MwALIGNMENT_BEGINNING,
+			 NULL);
+
+	MwLoop(window);
+}
