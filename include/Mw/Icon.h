959b6f4a60c381f49c83f594b35b82ce2fae6143
nishi <nishi@b9cfdab3-6d41-4d17-bbe4-086880011989>
2025-10-10 16:53:51 +0000

    icon
    git-svn-id: http://svn2.nishi.boats/svn/milsko/trunk@256 b9cfdab3-6d41-4d17-bbe4-086880011989

---
+++ include/Mw/Icon.h
@@ -0,0 +1,39 @@
+/* $Id$ */
+/*!
+ * %file Mw/Icon.h
+ * %brief Icon
+ */
+#ifndef __MW_ICON_H__
+#define __MW_ICON_H__
+
+#include <Mw/MachDep.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/*!
+ * %brief Info icon
+ */
+MWDECL char* MwIconInfo[];
+
+/*!
+ * %brief Note icon
+ */
+MWDECL char* MwIconNote[];
+
+/*!
+ * %brief Question icon
+ */
+MWDECL char* MwIconQuestion[];
+
+/*!
+ * %brief Warning icon
+ */
+MWDECL char* MwIconWarning[];
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
