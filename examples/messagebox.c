22b2d33b51f83750806aec9abf28f2663657c4f5
nishi <nishi@b9cfdab3-6d41-4d17-bbe4-086880011989>
2025-10-11 16:38:30 +0000

    enum
    git-svn-id: http://svn2.nishi.boats/svn/milsko/trunk@280 b9cfdab3-6d41-4d17-bbe4-086880011989

--- examples/messagebox.c
+++ examples/messagebox.c
@@ -15,13 +15,23 @@ void spawn2(MwWidget handle, void* user, void* call) {
 	MwAddUserHandler(MwMessageBoxGetChild(mb, MwMB_BUTTONOK), MwNactivateHandler, ok, mb);
 }
 
+void spawn3(MwWidget handle, void* user, void* call) {
+	int i;
+	for(i = 0; i <= 6; i++) {
+		MwWidget mb = MwMessageBox(user, "messagebox test", "title", i | MwMB_BUTTONOK);
+		MwAddUserHandler(MwMessageBoxGetChild(mb, MwMB_BUTTONOK), MwNactivateHandler, ok, mb);
+	}
+}
+
 int main() {
 	MwWidget msg  = MwVaCreateWidget(MwWindowClass, "test", NULL, MwDEFAULT, MwDEFAULT, 300, 100, MwNtitle, "test", NULL);
 	MwWidget btn  = MwVaCreateWidget(MwButtonClass, "button", msg, 8, 8, 300 - 16, (100 - 16) / 2, MwNtext, "press me!", NULL);
-	MwWidget btn2 = MwVaCreateWidget(MwButtonClass, "button", msg, 8, 8 + (100 - 16) / 2, 300 - 16, (100 - 16) / 2, MwNtext, "press me!", NULL);
+	MwWidget btn2 = MwVaCreateWidget(MwButtonClass, "button", msg, 8, 8 + (100 - 16) / 2, (300 - 16) / 2, (100 - 16) / 2, MwNtext, "press me!", NULL);
+	MwWidget btn3 = MwVaCreateWidget(MwButtonClass, "button", msg, 8 + (300 - 16) / 2, 8 + (100 - 16) / 2, (300 - 16) / 2, (100 - 16) / 2, MwNtext, "press me!", NULL);
 
 	MwAddUserHandler(btn, MwNactivateHandler, spawn, msg);
 	MwAddUserHandler(btn2, MwNactivateHandler, spawn2, msg);
+	MwAddUserHandler(btn3, MwNactivateHandler, spawn3, msg);
 
 	MwLoop(msg);
 }
