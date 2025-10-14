2b0c1734f9c2bb3408acf84f94f3759af7cb56f8
NishiOwO <nishi@nishi.boats>
2025-10-14 07:04:11 +0000

    improve image demo
    git-svn-id: http://svn2.nishi.boats/svn/milsko/trunk@318 b9cfdab3-6d41-4d17-bbe4-086880011989

--- examples/image.c
+++ examples/image.c
@@ -6,9 +6,11 @@ int main() {
 					     MwNtitle, "image image",
 					     NULL);
 	MwWidget   image  = MwCreateWidget(MwImageClass, "image", window, 50, 50, 200, 400);
-	MwWidget   image2 = MwCreateWidget(MwImageClass, "image", window, 250, 50, 200, 400);
+	MwWidget   image2 = MwCreateWidget(MwImageClass, "image", window, 250, 50, 200, 200);
+	MwWidget   image3 = MwCreateWidget(MwImageClass, "image", window, 250, 250, 200, 200);
 	MwLLPixmap px	  = MwLoadImage(window, "examples/picture.png");
 	MwLLPixmap px2	  = MwLoadImage(window, "examples/picture.jpg");
+	MwLLPixmap px3	  = MwLoadImage(window, "resource/logo.png");
 
 	MwVaApply(window,
 		  MwNiconPixmap, px,
@@ -19,6 +21,9 @@ int main() {
 		  NULL);
 	MwVaApply(image2,
 		  MwNpixmap, px2,
+		  NULL);
+	MwVaApply(image3,
+		  MwNpixmap, px3,
 		  NULL);
 
 	MwLoop(window);
