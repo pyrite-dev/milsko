61772bf9caf974cb53618dceae514efe18f7afcd
NishiOwO <nishi@nishi.boats>
2025-10-15 09:02:23 +0000

    yeah
    git-svn-id: http://svn2.nishi.boats/svn/milsko/trunk@337 b9cfdab3-6d41-4d17-bbe4-086880011989

---
+++ tools/harvard.pl
@@ -0,0 +1,14 @@
+#!/usr/bin/env perl
+# $Id$
+
+open(IN, "<", "resource/harvard.txt");
+print("const char* harvard[] = {\n");
+while(my $l = <IN>){
+	$l =~ s/\r?\n$//;
+	if($l =~ /^[ \t]*[0-9]+\. (.+)$/){
+		print("	\"" . $1 . "\",\n");
+	}
+}
+print("	NULL\n");
+print("};\n");
+close(IN);
