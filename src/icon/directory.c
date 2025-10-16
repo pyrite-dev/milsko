940c00eb459fc2b6c8753675fa3795e25cfc90dd
NishiOwO <nishi@nishi.boats>
2025-10-16 08:45:46 +0000

    icons
    git-svn-id: http://svn2.nishi.boats/svn/milsko/trunk@353 b9cfdab3-6d41-4d17-bbe4-086880011989

---
+++ src/icon/directory.c
@@ -0,0 +1,27 @@
+/* $Id: icon.sh 344 2025-10-15 16:17:47Z nishi $ */
+#include <Mw/Milsko.h>
+
+/* XPM */
+char *MwIconDirectory[] = {
+/* columns rows colors chars-per-pixel */
+"12 14 4 1 ",
+"  c black",
+". c #996633",
+"X c #FFCC99",
+"o c None",
+/* pixels */
+"      oooooo",
+"XXXXXX ooooo",
+"XXXXXXX     ",
+"XXXXXX.XXXXX",
+"......XXXXXX",
+"XXXXXXXXXXXX",
+"XXXXXXXXXXXX",
+"XXXXXXXXXXXX",
+"XXXXXXXXXXXX",
+"XXXXXXXXXXXX",
+"XXXXXXXXXXXX",
+"XXXXXXXXXXXX",
+"XXXXXXXXXXXX",
+"XXXXXXXXXXXX"
+};
