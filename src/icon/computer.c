95d57515525e2cbfd0ecb49942334e929091a3bf
NishiOwO <nishi@nishi.boats>
2025-10-16 18:52:20 +0000

    computer
    git-svn-id: http://svn2.nishi.boats/svn/milsko/trunk@376 b9cfdab3-6d41-4d17-bbe4-086880011989

---
+++ src/icon/computer.c
@@ -0,0 +1,32 @@
+/* $Id$ */
+#include <Mw/Milsko.h>
+
+/* XPM */
+char* MwIconComputer[] = {
+    /* columns rows colors chars-per-pixel */
+    "16 16 8 1 ",
+    "  c white",
+    ". c gray58",
+    "X c None",
+    "o c #292118",
+    "O c #5A6363",
+    "+ c #39A5CE",
+    "@ c #73BDDE",
+    "# c black",
+    /* pixels */
+    "XXXXXXXXXXXXXXXX",
+    "XXXXXXXXXXXXXXXX",
+    "XXXXXXXXXXXXXXXX",
+    "XX.OOOOOOOXXXXXX",
+    "XXOX.....XoXXXXX",
+    "XXOO..OOOX#.XXXX",
+    "XXOOX@@@+X#.XXXX",
+    "XXOO+@@@.X#.XXXX",
+    "XXOX..... #.XXXX",
+    "X+OOOOOOOOO+XXXX",
+    "@+@+++++++@+OXXX",
+    "@@++++++++++OXXX",
+    "X+..XOOOOOOO.XXX",
+    ".OXXXXXXXXXoOXXX",
+    "#.OOOOOOOOO.#.XX",
+    "O#ooooooooo#OXXX"};
