1175f7886b00bfb260f7aa4aef3b368ce8052e82
NishiOwO <nishi@nishi.boats>
2025-10-19 14:36:23 +0000

    doxygen support
    git-svn-id: http://svn2.nishi.boats/svn/milsko/trunk@418 b9cfdab3-6d41-4d17-bbe4-086880011989

---
+++ tools/doxyfilter.pl
@@ -0,0 +1,12 @@
+#!/usr/bin/env perl
+# $Id$
+
+open(IN, "<", $ARGV[0]);
+while(my $l = <IN>){
+	$l =~ s/\r?\n$//g;
+	$l =~ s/%([^ ]+)/@\1/g;
+	if(!($l =~ /\@prop /)){
+		print("$l\n");
+	}
+}
+close(IN);
--- tools/logo.sh
+++ tools/logo.sh
@@ -3,3 +3,4 @@
 povray +W256 +H256 +Ologo.png resource/logo.pov
 povray +W200 +H40 +Obanner.png resource/banner.pov
 mv logo.png banner.png resource/
+convert resource/logo.png -scale x64 resource/logo64.png
