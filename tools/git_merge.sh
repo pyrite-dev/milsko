d7234d3a117ce90c1aa826884c966a568449b9de
IoIxD <ioixd@users.noreply.github.com>
2025-10-20 22:04:44 +0000

    convience script for merging PRs from the git repo to svn
    git-svn-id: http://svn2.nishi.boats/svn/milsko/trunk@436 b9cfdab3-6d41-4d17-bbe4-086880011989

---
+++ tools/git_merge.sh
@@ -0,0 +1,7 @@
+if [[ $1 -eq "" ]]; then
+    echo "Blank argument."
+    exit
+fi
+
+echo "Applying PR #$1    (https://github.com/pyrite-dev/milsko/pull/$1.diff)"
+curl https://github.com/pyrite-dev/milsko/pull/$1.diff -L | patch -p1
