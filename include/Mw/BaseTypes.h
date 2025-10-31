8a3e9b3cd66a459a2bd10eb66d96c58307b7989d
NishiOwO <nishi@nishi.boats>
2025-10-31 20:29:01 +0000

    rename
    git-svn-id: http://svn2.nishi.boats/svn/milsko/trunk@534 b9cfdab3-6d41-4d17-bbe4-086880011989

--- include/Mw/BaseTypes.h
+++ include/Mw/BaseTypes.h
@@ -17,16 +17,18 @@ typedef unsigned char MwBool;
 #define MwFALSE ((MwBool)0)
 
 #if __STDC_VERSION__ >= 199901L || __GNUC__ > 2
-typedef long long MwOffset;
-typedef unsigned long long MwUOffset;
+typedef long long MwI64;
+typedef unsigned long long MwU64;
 #elif defined(_MSC_VER) || defined(__BORLANDC__) || defined(__WATCOMC__)
-typedef __int64 MwOffset;
-typedef unsigned __int64 MwUOffset;
+typedef __int64 MwI64;
+typedef unsigned __int64 MwU64;
 #else
 /* out of hope */
-typedef long MwOffset;
-typedef unsigned long MwUOffset;
+typedef long MwI64;
+typedef unsigned long MwU64;
 #endif
+
+typedef MwI64 MwOffset;
 
 struct _MwPoint {
 	int x;
