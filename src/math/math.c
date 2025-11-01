4add837d511a7bbf1b0ed9ee9f75609fb40f15a1
IoIxD <alphaproject217@gmail.com>
2025-10-31 21:13:05 -0700

    cpuid

--- src/math/math.c
+++ src/math/math.c
@@ -38,24 +38,23 @@ switch(ty) {
 	return vec;
 }
 
-static MwBool hasMMX(void) {
-	MwU32 _eax = 1;
-	MwU32 _ebx, _edx;
+static cpuFeatures getCPUFeatures(void) {
+	MwU32	    _eax = 1;
+	cpuFeatures _edx;
 
 #ifdef __WATCOMC__
 	__asm {
 		cpuid
 		mov _eax, eax
-		mov _ebx, ebx
 		mov _edx, edx
 	}
 #else
 	__asm__ __volatile__(
-	    "cpuid" : "=a"(_eax), "=b"(_ebx), "=d"(_edx)
+	    "cpuid" : "=a"(_eax), "=d"(_edx)
 	    : "a"(1));
 #endif
 
-	return (_edx & (1 << 23)) == (1 << 23);
+	return _edx;
 }
 
 static MwLLMathVTable** mwLLMultiTable;
@@ -69,10 +68,19 @@ static MwLLMathVTable* getMultiTable(MwLLVecType ty) {
 }
 
 static MwLLMathVTable* multiTableSetupAndGet(MwLLVecType ty) {
+#if defined(__i386__) || defined(__x86_64__)
+	cpuFeatures features;
+#endif
 	mwLLMultiTable = default_multi_table();
 
 #if defined(__i386__) || defined(__x86_64__)
-	if(hasMMX()) {
+	features = getCPUFeatures();
+	printf("Avaliable x86_64 Features:\n");
+	printf("\tMMX: %s\n", features.mmx ? "true" : "false");
+	printf("\tSSE: %s\n", features.sse ? "true" : "false");
+	printf("\tSSE2: %s\n", features.sse2 ? "true" : "false");
+
+	if(features.mmx) {
 		mmx_apply(mwLLMultiTable);
 	}
 #endif
--- src/math/math_internal.h
+++ src/math/math_internal.h
@@ -3,7 +3,40 @@
 #ifndef __MW_LOWLEVEL_INTERNAL_MATH_H__
 #define __MW_LOWLEVEL_INTERNAL_MATH_H__
 
+#include <Mw/BaseTypes.h>
 #include <Mw/LowLevelMath.h>
+
+struct _cpuFeatures {
+	MwBool fpu : 1;
+	MwBool vme : 1;
+	MwBool de : 1;
+	MwBool pse : 1;
+	MwBool tsc : 1;
+	MwBool msr : 1;
+	MwBool pae : 1;
+	MwBool mce : 1;
+	MwBool cx8 : 1;
+	MwBool apic : 1;
+	MwBool sep : 1;
+	MwBool mtrr : 1;
+	MwBool pge : 1;
+	MwBool mca : 1;
+	MwBool cmov : 1;
+	MwBool pat : 1;
+	MwBool pse36 : 1;
+	MwBool psn : 1;
+	MwBool clflush : 1;
+	MwBool ds : 1;
+	MwBool acpi : 1;
+	MwBool mmx : 1;
+	MwBool fxsr : 1;
+	MwBool sse : 1;
+	MwBool sse2 : 1;
+	MwBool ss : 1;
+	MwBool hit : 1;
+	MwBool tm : 1;
+	MwBool pbe : 1;
+};
 
 struct _MwLLMathVTable {
 	void (*Add)(MwLLVec* a, MwLLVec* b, MwLLVec* out);
@@ -21,6 +54,7 @@ struct _MwLLMathVTable {
 };
 
 typedef struct _MwLLMathVTable MwLLMathVTable;
+typedef struct _cpuFeatures    cpuFeatures;
 
 MwLLMathVTable** default_multi_table(void);
 void		 mmx_apply(MwLLMathVTable**);
