d96b74c1b14b5624d9d032d38210dc5a2034d10f
NishiOwO <nishi@nishi.boats>
2025-11-11 10:54:49 +0000

    fix for gdi case
    git-svn-id: http://svn2.nishi.boats/svn/milsko/trunk@660 b9cfdab3-6d41-4d17-bbe4-086880011989

--- src/widget/scrollbar.c
+++ src/widget/scrollbar.c
@@ -130,7 +130,7 @@ static void draw(MwWidget handle) {
 }
 
 static void mouse_move(MwWidget handle) {
-	int or		 = MwGetInteger(handle, MwNorientation);
+	int	     or	 = MwGetInteger(handle, MwNorientation);
 	scrollbar_t* scr = handle->internal;
 
 	if(!handle->pressed) return;
@@ -158,9 +158,9 @@ static void mouse_move(MwWidget handle) {
 }
 
 static void mouse_down(MwWidget handle, void* ptr) {
-	int ww		 = MwGetInteger(handle, MwNwidth);
-	int wh		 = MwGetInteger(handle, MwNheight);
-	int or		 = MwGetInteger(handle, MwNorientation);
+	int	     ww	 = MwGetInteger(handle, MwNwidth);
+	int	     wh	 = MwGetInteger(handle, MwNheight);
+	int	     or	 = MwGetInteger(handle, MwNorientation);
 	scrollbar_t* scr = handle->internal;
 	MwLLMouse*   m	 = ptr;
 
