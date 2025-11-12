1cd4be6b4c44edd8a3d7c29143395dd0b485e2bb
NishiOwO <nishi@nishi.boats>
2025-11-12 01:58:49 +0000

    new
    git-svn-id: http://svn2.nishi.boats/svn/milsko/trunk@669 b9cfdab3-6d41-4d17-bbe4-086880011989

--- tools/logo.sh
+++ tools/logo.sh
@@ -3,5 +3,6 @@
 povray +W256 +H256 +Ologo.png resource/logo/logo.pov
 povray +W200 +H40 +Obanner.png resource/logo/banner.pov
 povray +W1280 +H640 +Obanner2.png resource/logo/banner.pov
-mv logo.png banner.png banner2.png resource/logo/
+povray +W400 +H80 +Obanner3.png resource/logo/banner.pov
+mv logo.png banner.png banner2.png banner3.png resource/logo/
 magick resource/logo/logo.png -scale x64 resource/logo/logo64.png
