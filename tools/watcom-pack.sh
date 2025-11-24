2af2759e55d000e404e18d2fc084599325a1b595
NishiOwO <nishi@nishi.boats>
2025-11-24 21:31:53 +0000

    add tool
    git-svn-id: http://svn2.nishi.boats/svn/milsko/trunk@803 b9cfdab3-6d41-4d17-bbe4-086880011989

---
+++ tools/watcom-pack.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+# $Id$
+set -e
+wmake -f WatMakefile
+rm -rf milsko
+mkdir -p milsko-examples
+cp src/Mw.dll milsko-examples/
+cd examples/basic
+for i in *.c; do
+	owcc -bnt -I../../include ../../src/Mw.lib -o../../milsko-examples/`echo $i | cut -d. -f1`.exe $i
+done
+cd ../..
+cd examples/gldemos
+for i in *.c; do
+	if [ "$i" = "glutlayer.c" ]; then
+		continue
+	fi
+	owcc -bnt -I../../include ../../src/Mw.lib -o../../milsko-examples/gl`echo $i | cut -d. -f1`.exe $i opengl32.lib glu32.lib
+done
+cd ../..
+rm -f milsko-examples.zip
+zip -rv milsko-examples.zip milsko-examples
+rm -rf milsko-examples
