712a640f9df1b19f33bde1a608954acdd6aaceff
Nishi <nishi@nishi.boats>
2026-04-16 10:47:24 +0900

    probably it is better to do this way

--- src/widget/opengl.c
+++ src/widget/opengl.c
@@ -381,7 +381,7 @@ static void mwOpenGLMakeCurrentImpl(MwWidget handle) {
 #ifdef USE_GDI
 	if(handle->lowlevel->common.type == MwLLBackendGDI) {
 		gdiopengl_t* o		       = handle->internal;
-		int (__stdcall *swap_interval_ext)(int) = NULL;
+		BOOL (APIENTRY *swap_interval_ext)(int) = NULL;
 
 		o->wglMakeCurrent(o->dc, o->gl);
 
