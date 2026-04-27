d909812891ce5e7891b5ccb046bdeb6123c72701
IoIxD <alphaproject217@gmail.com>
2026-04-27 12:39:23 -0700

    i think this is probably needed for doing wayland on freebsd?

--- pl/ostype/FreeBSD.pl
+++ pl/ostype/FreeBSD.pl
@@ -1,7 +1,14 @@
 add_incdir2("-I/usr/local/include");
 add_libdir(
     "-L/usr/local/lib -Wl,-R/usr/local/lib");
-use_backend("x11");
+
+if (param_get("wayland") && not(param_get("tiny"))) {
+    push(@enabled_backends, "wayland");
+}
+if (param_get("x11")) {
+    push(@enabled_backends, "x11");
+}
+
 add_libs("-lpthread");
 
 add_cflags("-DUSE_DBUS");
