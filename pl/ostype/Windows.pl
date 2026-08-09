df1972e91c923dda68b01561d16c29ae299eb69a
Nishi <nishi@nishi.boats>
2026-08-10 01:55:27 +0900

    remove winmm

--- pl/ostype/Windows.pl
+++ pl/ostype/Windows.pl
@@ -4,7 +4,6 @@ $executable_suffix = ".exe";
 $math              = "";
 add_ldflags("-Wl,--out-implib,src/libMw.dll.a -static-libgcc");
 use_backend("gdi");
-add_libs("-lwinmm");
 add_libs("-lole32");
 add_libs("-luuid");
 
