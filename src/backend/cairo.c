66ea53f5231baa8230c98c155505ea05fa27807a
Nishi <nishi@nishi.boats>
2026-09-14 07:15:12 +0900

    fix

--- src/backend/cairo.c
+++ src/backend/cairo.c
@@ -27,6 +27,7 @@ void MwLLCairoLine(struct _MwLLCairo handle, MwPoint* points, MwLLColor color) {
 
 	cairo_set_antialias(handle.front_cairo_back, CAIRO_ANTIALIAS_NONE);
 	cairo_set_line_cap(handle.front_cairo_back, CAIRO_LINE_CAP_SQUARE);
+	cairo_set_line_width(handle.front_cairo_back, 1);
 	cairo_set_source_rgba(handle.front_cairo_back, color->common.red / 255.0, color->common.green / 255.0, color->common.blue / 255.0, 1.0);
 	cairo_new_path(handle.front_cairo_back);
 	for(i = 0; i < 2; i++) {
